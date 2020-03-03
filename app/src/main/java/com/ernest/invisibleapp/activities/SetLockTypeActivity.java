c2c1160fcf86c00db36bd5c026bc663ae4eb70c0
Ernest <youandinews@gmail.com>
2020-03-03 14:11:02 +0100

    Pattern Lock Implemented

---
+++ app/src/main/java/com/ernest/invisibleapp/activities/SetLockTypeActivity.java
@@ -0,0 +1,16 @@
+package com.ernest.invisibleapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ernest.invisibleapp.R;
+
+public class SetLockTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_lock_type);
+    }
+}
